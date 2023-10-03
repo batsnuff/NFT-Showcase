672e2307dfbbc52a161e64368fca269d8982cdcb
batsnuff <cryptobts7@gmail.com>
2023-10-03 12:33:25 +0200

    fixed code, only one click

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 let count = 0;
 const counterElement = document.getElementById('counter');
 const incrementButton = document.getElementById('incrementButton');
+const thankYouMessage = document.getElementById('thankYouMessage'); // Dodaj element dla komunikatu
 
 function updateCounter() {
   counterElement.textContent = count;
 }
 
 function incrementCounter() {
-  count++;
-  updateCounter();
-  
-  // Store the count value in localStorage
-  localStorage.setItem('visitorCount', count);
+  if (count === 0) { // Sprawdź, czy licznik wynosi 0 (czyli użytkownik jeszcze nie kliknął)
+    count++;
+    updateCounter();
+    
+    // Store the count value in localStorage
+    localStorage.setItem('visitorCount', count);
+    
+    // Wyświetl komunikat "Thank you! 😁"
+    thankYouMessage.textContent = 'Thank you! 😁';
+  }
 }
 
 incrementButton.addEventListener('click', incrementCounter);
@@ -23,3 +29,8 @@ if (localStorage.getItem('visitorCount')) {
 
 // Update the counter value on the page
 updateCounter();
+
+// Sprawdź, czy użytkownik już kliknął, i wyświetl komunikat
+if (count > 0) {
+  thankYouMessage.textContent = 'Thank you! 😁';
+}
